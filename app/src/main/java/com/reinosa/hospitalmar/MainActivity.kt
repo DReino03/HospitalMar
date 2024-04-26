8a392e0dd6eff980377150efd2ed5df712725dca
gesa <gerard.sanchez.7e6@itb.cat>
2024-04-26 17:09:16 +0200

    perfil update

--- app/src/main/java/com/reinosa/hospitalmar/MainActivity.kt
+++ app/src/main/java/com/reinosa/hospitalmar/MainActivity.kt
@@ -71,7 +71,7 @@ fun Navigation(navController: NavHostController) {
     val loginViewModel : LoginViewModel = remember { LoginViewModel() }
     NavHost(
         navController = navController,
-        startDestination = NavigationGraph.LOGIN
+        startDestination = NavigationGraph.PROFILE
     ) {
         composable(NavigationGraph.HOME) {
             HomeScreen(navController)
