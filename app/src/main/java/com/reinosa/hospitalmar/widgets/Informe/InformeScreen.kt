b1de63d8f124d53200420c5d23585b8592d1947f
DReino03 <daniel.reinosa.7e6@itb.cat>
2024-05-16 23:01:10 +0200

    Comments in InformeScreen

--- app/src/main/java/com/reinosa/hospitalmar/widgets/Informe/InformeScreen.kt
+++ app/src/main/java/com/reinosa/hospitalmar/widgets/Informe/InformeScreen.kt
@@ -6,36 +6,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.DrawerValue
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.rememberDrawerState
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.reinosa.hospitalmar.R
 import com.reinosa.hospitalmar.ViewModel.LoginViewModel
-import com.reinosa.hospitalmar.widgets.Drawer.DrawerHeader
-import com.reinosa.hospitalmar.widgets.Drawer.DrawerItems
-import kotlinx.coroutines.launch
+
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun InformeScreen(navController: NavController, loginViewModel: LoginViewModel) {
+fun InformeScreen(navController: NavController, viewModel: LoginViewModel) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
@@ -44,13 +26,13 @@ fun InformeScreen(navController: NavController, loginViewModel: LoginViewModel) 
             Spacer(modifier = Modifier
                 .height(16.dp)
             )
-            androidx.compose.material3.Text(
+            Text(
                 text = "Qualificacions:",
                 style = MaterialTheme.typography.h5
             )
             LazyColumn {
                 items(10) {
-                    CardInfo()
+                    CardInfo(viewModel)
                 }
 
             }
